93f35405a181d7bcf9d0aebdfa3bc6140bee79fe
Olga Brani <olgabrani@grnet.gr>
2026-02-25 15:22:30 +0200

    Configure Github Pages Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
+  site: 'https://olgabrani.github.io',
+  base: '/olga_b_draws',
   output: 'static',
-  adapter: vercel(),
   integrations: [
     partytown({
       config: {
